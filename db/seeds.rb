74be8bfc39d823a3e9b5c03959321e3fd4befdfb
Josue Araujo <jaraujo@metricas.mx>
2020-01-22 00:54:31 -0600

    * Added records on seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Carrier.create({name: 'FEDEX'})
+
+DistanceUnit.create({key:'CM', name:'Centimeters', description: 'Centimeters'})
+DistanceUnit.create({key:'IN', name:'Inches', description: 'Inches'})
+
+MassUnit.create({key:'KG', name:'Kilograms', description: 'Kilograms'})
+MassUnit.create({key:'LB', name:'Pounds', description: 'Pounds'})
